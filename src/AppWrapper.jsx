bc2036949088637df60f5100647b2d51bb94fbb1
Ryan Schmitt <ryan15schmitt@gmail.com>
2025-02-23 14:20:08 -0800

    Fixed router

--- src/AppWrapper.jsx
+++ src/AppWrapper.jsx
@@ -1,16 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Make sure you're importing these
+import { BrowserRouter, Route, Routes } from "react-router-dom"; // Make sure you're importing these
 import App from "./App"; // Assuming App is your main component
 import Posts from "./Posts"; // Assuming Posts is your component for the posts page
 
 function AppWrapper() {
   return (
-    <Router>
+    <BrowserRouter basename="/WhatsTheMove">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/posts" element={<Posts />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
